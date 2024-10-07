8aad2c0585e8acc9cc2f8533f0be7e9e3d77ab13
mohsyn007 <mohsinalam1141@gmail.com>
2024-10-07 21:54:08 +0600

    navigation

--- scripts/video.js
+++ scripts/video.js
@@ -2,19 +2,37 @@ console.log("video scripts added");
 // 1. Fetch ,load and catagories on html
 //create load catagories
 const loadCategories = () => {
-    
+
     // fetch data
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    .then(res=>res.json())
-    .then(data=>displayCategories(data.categories))
-    .catch((error)=>console.log(error))
+        .then(res => res.json())
+        .then(data => displayCategories(data.categories))
+        .catch((error) => console.log(error))
 
 
 }
 
-loadCategories();
+// {category_id: '1001', category: 'Music'}
+// category
+// : 
+// "Music"
+// category_id
+// : 
+// "1001"
+
+
 //create display catagories
-const displayCategories = () => {
-    console.log("categories create");
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById("categories");
+    categories.forEach((item) => {
+        console.log(item);
+        // create button
+        const button = document.createElement("button");
+        button.classList = "btn";
+        button.innerText = item.category;
+        // add button to category container
+        categoryContainer.append(button);
+    })
 
 }
+loadCategories();
